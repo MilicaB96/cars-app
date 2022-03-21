a308420b6cd5073149277c7704bb553618db6c3f
Milica <magiflowerchild@gmial.com>
2022-03-21 21:51:41 +0100

    added cars listing

--- src/pages/AppCars.js
+++ src/pages/AppCars.js
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import CarService from "../services/CarService";
 function AppCars() {
-  return <></>;
+  const [cars, setCars] = useState([]);
+  async function getCars() {
+    const data = await CarService.getAll();
+    console.log(data);
+    setCars(data);
+  }
+  useEffect(() => {
+    getCars();
+  }, []);
+  return (
+    <div>
+      <h1>Cars:</h1>
+      <ul>
+        {cars.map((car) => {
+          <li>{car}</li>;
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default AppCars;
---
+++ src/services/CarService.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+class CarService {
+  constructor() {
+    const instance = axios.create({
+      baseURL: "http://localhost:5500",
+    });
+
+    this.client = instance;
+  }
+
+  async getAll() {
+    try {
+      const { data } = await this.client.get("api/cars");
+      return data;
+    } catch (error) {
+      console.log("Greska", error);
+    }
+
+    return [];
+  }
+}
+
+export default new CarService();
